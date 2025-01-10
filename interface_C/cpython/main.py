e7c108f139ae22e9999cfb546153b5b0be4276b5
Paul-Ernest <paulernestmartin@gmail.com>
2025-01-10 17:56:32 +0100

    *add : cpython

---
+++ interface_C/cpython/main.py
@@ -0,0 +1,4 @@
+import extensions
+
+resultat = extensions.somme(3.5, 4.5)
+print(resultat)
---
+++ interface_C/cpython/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, Extension
+
+# Define the C extension
+module = Extension('extensions', sources=['extensions/extension.c'])
+
+# Call setup to build the package
+setup(
+    name='extensions-c',
+    version='1.0.0',
+    description='demonstration extensions c avec cpython',
+    ext_modules=[module],  # Include the C extension
+)
